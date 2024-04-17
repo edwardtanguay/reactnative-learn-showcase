371cc0837e938e5338471019e33780950e9fa9ea
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-17 15:34:05 +0200

    box1, box2

--- App.tsx
+++ App.tsx
@@ -9,9 +9,15 @@ import {
 
 const App = () => {
 	return (
-		<SafeAreaView>
+		<SafeAreaView style={{flex: 1}}>
 			<View style={styles.container}>
-				<Text>Hello World.</Text>
+				<Text style={styles.text}>Here are some styles:</Text>
+				<View style={styles.box1}>
+					<Text>Box 1</Text>
+				</View>
+				<View style={styles.box2}>
+					<Text>Box 2</Text>
+				</View>
 			</View>
 		</SafeAreaView>
 	);
@@ -20,12 +26,35 @@ const App = () => {
 const styles = StyleSheet.create({
 	container: {
 		marginLeft: 30,
-		marginTop: 40,
+		marginVertical: 40,
 		marginRight: 30,
 		backgroundColor: "#ccc",
 		padding: 10,
+		borderRadius: 5,
+		borderColor: 'navy',
+		borderWidth: 2,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1
+	},
+	text: {
+		fontSize: 25,
+		fontWeight: 'bold'
+	},
+	box1: {
+		marginTop: 10,
+		backgroundColor: 'teal',
+		width: '90%',
+		padding: 10,
 		borderRadius: 5
 	},
+	box2: {
+		marginTop: 10,
+		backgroundColor: 'darkorange',
+		width: '90%',
+		padding: 10,
+		borderRadius: 5
+	}
 });
 
 export default App;
