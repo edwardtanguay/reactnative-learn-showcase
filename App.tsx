47f260418eb954b7d222bbf5d6b885d68239bf95
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-17 16:16:18 +0200

    basic color box

--- App.tsx
+++ App.tsx
@@ -6,18 +6,23 @@ import {
 	StyleSheet,
 	KeyboardAvoidingView,
 } from "react-native";
+import { ColorBox } from "./components/ColorBox";
 
 const App = () => {
 	return (
-		<SafeAreaView style={{flex: 1}}>
+		<SafeAreaView style={{ flex: 1 }}>
 			<View style={styles.container}>
 				<Text style={styles.text}>Here are some styles:</Text>
-				<View style={styles.box1}>
+				<View style={[styles.box, styles.box1]}>
 					<Text>Box 1</Text>
 				</View>
-				<View style={styles.box2}>
+				<View style={[styles.box, styles.box2]}>
 					<Text>Box 2</Text>
 				</View>
+				<View style={[styles.box, styles.box3]}>
+					<Text style={styles.box3text}>Box 3</Text>
+				</View>
+				<ColorBox/>
 			</View>
 		</SafeAreaView>
 	);
@@ -31,29 +36,35 @@ const styles = StyleSheet.create({
 		backgroundColor: "#ccc",
 		padding: 10,
 		borderRadius: 5,
-		borderColor: 'navy',
+		borderColor: "navy",
 		borderWidth: 2,
-		alignItems: 'center',
-		justifyContent: 'center',
-		flex: 1
+		alignItems: "center",
+		justifyContent: "center",
+		flex: 1,
 	},
 	text: {
 		fontSize: 25,
-		fontWeight: 'bold'
+		fontWeight: "bold",
+	},
+	box: {
+		marginTop: 10,
+		width: "90%",
+		padding: 10,
+		borderRadius: 5,
+		borderWidth: 2,
+		borderColor: '#555'
 	},
 	box1: {
-		marginTop: 10,
-		backgroundColor: 'teal',
-		width: '90%',
-		padding: 10,
-		borderRadius: 5
+		backgroundColor: "teal",
 	},
 	box2: {
-		marginTop: 10,
-		backgroundColor: 'darkorange',
-		width: '90%',
-		padding: 10,
-		borderRadius: 5
+		backgroundColor: "darkorange",
+	},
+	box3: {
+		backgroundColor: "navy"
+	},
+	box3text: {
+		color: '#ddd'
 	}
 });
 
