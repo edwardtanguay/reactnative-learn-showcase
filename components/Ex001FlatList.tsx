ebc17103f3f21b0d1f607696d0db8672e17652dc
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-17 20:03:58 +0200

    showing frameworks

--- components/Ex001FlatList.tsx
+++ components/Ex001FlatList.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const frameworks = ["React", "Next.js", "SvelteKit", "Nuxt", "Angular"];
 
 export const Ex001FlatList = () => {
 	return (
 		<View style={styles.container}>
-			<Text>flat list</Text>
+			<Text style={styles.title}>Frameworks: </Text>
+			<FlatList
+				data={frameworks}
+				keyExtractor={item => item}
+				renderItem={data => <Text>{data.item}</Text>}
+			></FlatList>
 		</View>
 	);
 };
@@ -13,4 +20,7 @@ const styles = StyleSheet.create({
 	container: {
 		marginTop: 20,
 	},
+	title: {
+		fontWeight: 'bold'
+	}
 });
