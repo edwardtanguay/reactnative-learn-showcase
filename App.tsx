bc63ac9314ccf4aa3c1533a40aa13f833daf94c1
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-17 15:00:58 +0200

    hello world padding

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+	View,
+	Text,
+	SafeAreaView,
+	StyleSheet,
+	KeyboardAvoidingView,
+} from "react-native";
 
 const App = () => {
 	return (
-		<View>
-			<Text>Hello World.</Text>
-		</View>
+		<SafeAreaView>
+			<View style={styles.container}>
+				<Text>Hello World.</Text>
+			</View>
+		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		marginLeft: 30,
+		marginTop: 40,
+		marginRight: 30,
+		backgroundColor: "#ccc",
+		padding: 10,
+		borderRadius: 5
+	},
+});
 
 export default App;
