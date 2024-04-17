86656cb9d16ce290038cacdfb42bf0cf33bb19fd
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-17 22:32:17 +0200

    installed navigation

--- components/Ex001FlatList.tsx
+++ components/Ex001FlatList.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 const frameworks = ["React", "Next.js", "SvelteKit", "Nuxt", "Angular"];
 
 export const Ex001FlatList = () => {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Ex001FlatList: </Text>
-			<FlatList
-				data={frameworks}
-				keyExtractor={item => item}
-				renderItem={data => <Text>{data.item}</Text>}
-			></FlatList>
-		</View>
+		<NavigationContainer>
+			<View style={styles.container}>
+				<Text style={styles.title}>Ex001FlatList: </Text>
+				<FlatList
+					data={frameworks}
+					keyExtractor={(item) => item}
+					renderItem={(data) => <Text>{data.item}</Text>}
+				></FlatList>
+			</View>
+		</NavigationContainer>
 	);
 };
 
@@ -21,6 +24,6 @@ const styles = StyleSheet.create({
 		marginTop: 20,
 	},
 	title: {
-		fontWeight: 'bold'
-	}
+		fontWeight: "bold",
+	},
 });
